4dcf26bf3cedef8d8dbb66de76679020a9dce0a9
Rohan Pagare <rohanpagare7599@gmail.com>
2026-06-07 11:57:01 +0530

    create register user

---
+++ auth/src/routes/register-user.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express';
+import {User} from "../models/user-schema";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("register", async (req: Request, res: Response) => {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
+    }
+    const newUser = User.build({
+        username,
+        email,
+        password
+    });
+    await newUser.save();
+    const userJwt = jwt.sign(
+        {
+            id: newUser!.id,
+            email: newUser.email
+        },
+        process.env.JWT_KEY!
+    )
+    req.session = {
+      jwt: userJwt,
+    };
+    res.status(201).send({ message: "success", data: newUser });
+})
