d402fe066a0abe950d08ae51d995ba7d86451cb1
Rohan Pagare <rohanpagare7599@gmail.com>
2026-06-07 23:59:59 +0530

    working on get api for board

---
+++ task/src/routes/get-all-boards.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import { requireAuth } from "../middlewares/require-auth";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
+import { Board } from "../models/board-schema";
+
+const router = express.Router();
+
+router.get("/api/board", requireAuth, async (req: Request, res: Response) => {
+  const userId = req.currentUser!.id;
+  const boards = await Board.find({
+    $where: { user_id: userId },
+  }).lean();
+  res.status(201).send({ message: "success", data: boards });
+});
+
+export { router as createBoardRouter };
---
+++ task/src/routes/get-board-details.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from "express";
+import { requireAuth } from "../middlewares/require-auth";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
+import { Board } from "../models/board-schema";
+import mongoose from "mongoose";
+import { Task } from "../models/task-schema";
+
+const router = express.Router();
+
+router.get(
+  "/api/board/:boardId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { boardId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(boardId as string)) {
+      return res.status(400).send({ message: "Invalid Board Id" });
+    }
+    const board = await Board.findById(boardId).lean();
+    if (!board) {
+      return res.status(404).send({ message: "Board Not Found." });
+    }
+    const currentUser = req.currentUser!.id;
+    if (board.user_id !== currentUser) {
+      return res.status(401).send({ message: "Not Autherized." });
+    }
+    const tasks = await Task.find({
+      $where: {
+        board_id: board.id,
+      },
+    }).lean();
+    res.status(200).send({ message: "success", data: { ...board, tasks } });
+  },
+);
+
+export { router as createBoardRouter };
